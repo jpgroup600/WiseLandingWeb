907263265ebbf7d3357b32d272805efd7915d9dc
jpgroup600 <133649099+jpgroup600@users.noreply.github.com>
2025-02-21 19:01:22 +0900

    added mt

--- app/components/body.tsx
+++ app/components/body.tsx
@@ -21,10 +21,10 @@ export default function Body() {
         <ProfileList />
       </div>
 
-      <div className="w-full flex items-center  md:mt-24 justify-center my- md:my-12">
+      <div className="w-full flex items-center max-md:my-10 md:mt-24 justify-center my- md:my-12">
         <div className="w-full md:w-[100%] bg-[#f2f2f2] py-4  "> 
           <h1 className="text-xl md:text-3xl font-bold text-black text-center nexonReg-font
-          max-md:my-10
+          max-md:my-2
           ">
             기존에 작업한게 있나요?
           </h1>
